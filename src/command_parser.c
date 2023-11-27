1e907dc1405fabc205ae52e549e29823c1b71061
Adeimn <103148586+Adeimn@users.noreply.github.com>
2023-11-28 00:09:35 +0100

    command_parser ok, command exec semi ok(manque implémentation des fonctions builtin)

---
+++ src/command_parser.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int parse_command(char *command, char **tokens) {
+    char *token = strtok(command, " ");
+    int i = 0;
+    while (token != NULL) {
+        tokens[i] = token;
+        token = strtok(NULL, " ");
+        i++;
+    }
+    tokens[i] = NULL;
+    return 0;
+}
+
+
---
+++ src/command_parser.h
@@ -0,0 +1,6 @@
+#ifndef COMMAND_PARSER_H
+#define COMMAND_PARSER_H
+
+int parse_command(char *command, char **tokens);
+
+#endif
