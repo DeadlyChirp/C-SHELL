7d669064285b4d259226f7b06c8f4e0822ecc1f6
aureglia <aureglia.christophe@gmail.com>
2023-12-21 14:36:07 +0100

    correction next

--- src/include/job.h
+++ src/include/job.h
@@ -6,7 +6,6 @@
 
 // Definition of the job structure
 struct job {
-  struct job *next;           /* next active job */
     int bg; // 0 = foreground, 1 = background 
     int id; //numero du job
     pid_t pgid; // process group id
